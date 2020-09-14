c46085f7eb5a7928fe24307088ef883533e2cb95
marcus.jarlevid@gmail.com <marcus.jarlevid@gmail.com>
2020-09-14 10:02:14 +0200

    Add project files.

---
+++ Net.core-fundamentals-2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Net.core_fundamentals_2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+         
+
+        }
+
+        static void Arrays()
+        {
+            int[,] multiTable = new int[10, 10];
+
+            foreach (var item in multiTable)
+            {
+                Console.WriteLine(item);
+            }
+
+            SetMultiNumbers(multiTable);
+
+            Console.WriteLine("after method call");
+
+            foreach (var item in multiTable)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+
+        static void SetMultiNumbers(int[,] table)
+        {
+            for (int y= 0; y < 10; y++)
+            {
+                for (int x = 0; x < 10; x++)
+                {
+                    table[y, x] = (y + 1) * (x + 1); 
+                }
+            }
+        }
+    }
+}
