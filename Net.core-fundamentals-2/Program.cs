878a5c2da117c9bc57712e1a744d34f922cea636
marcus.jarlevid@gmail.com <marcus.jarlevid@gmail.com>
2020-09-14 12:18:00 +0200

    mondays lecture

--- Net.core-fundamentals-2/Program.cs
+++ Net.core-fundamentals-2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 
 namespace Net.core_fundamentals_2
@@ -8,13 +9,79 @@ namespace Net.core_fundamentals_2
 
         static void Main(string[] args)
         {
-            Arrays();
+            // Arrays();
+            // PrimitivNumber();
+
+
+            AskForNumber();
 
         }
 
+        private static void AskForNumber()
+        {
+            bool notNumber = false;
+
+            do
+            {
+                Console.WriteLine("please input number");
+
+                double number = 0;
+
+                try
+                {
+                    number = double.Parse(Console.ReadLine());
+                    //will not go past here if there is an exception.
+                    notNumber = false;
+                } 
+                catch (ArgumentNullException)
+                {
+                    Console.WriteLine("did not get input.");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("The number was too damn high");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Was unable to read the number.\nYou cannot use text");
+                }
+                catch
+                {
+                    Console.WriteLine("sumting went wrong.");
+                    notNumber = true;
+                }
+                finally // will allways execute
+                {
+                    //Console.WriteLine("Number was:" + number);
+                    //Console.WriteLine("Number was: {0}", number);
+                    Console.WriteLine($"Number was: {number}");
+                }
+
+            } while (notNumber);
+        }
+
+        static void PrimitivNumber()
+        {
+            double number = 0.0;
+
+            Console.WriteLine(number);
+
+            CountUpNumber(number);
+        }
+
+        static void CountUpNumber(double num)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                num++;
+            }
+        }
+
+        //--------------------------------------------------------------------------------------
+
         static void Arrays()
         {
-            int[,] multiTable = new int[10, 10];
+            int[,] multiTable = new int[5, 5];
 
             foreach (var item in multiTable)
             {
@@ -42,5 +109,14 @@ namespace Net.core_fundamentals_2
                 }
             }
         }
+        //----------------------------------------------------------------------------
+        static void ListMethod()
+        {
+            List<double> numbersList = new List<double>();
+
+            numbersList.Add(5.5);
+        }
     }
+
+
 }
